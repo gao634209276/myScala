357055ac3f1f2a0202a5ed8d7c0c1c7293496fa8
noah <634209276@qq.com>
2017-06-23 19:57:49 +0800

    update

--- src/main/scala/chapter1/control/For.scala
+++ src/main/scala/chapter1/control/For.scala
@@ -4,36 +4,33 @@ package chapter1.control
   * Created by hadoop on 16-10-2.
   */
 object For {
-	//利用if替代while控制结构
-	//这些做可以减少var变量的使用，程序结构也更简单，表达能力更强
-	def gcd(x: Long, y: Long): Long =
-		if (y == 0) x else gcd(y, x % y)
+  //利用if替代while控制结构
+  //这些做可以减少var变量的使用，程序结构也更简单，表达能力更强
+  def gcd(x: Long, y: Long): Long =
+  if (y == 0) x else gcd(y, x % y)
 
-	//在for循环结构中还可以加入if进行过滤操作
-	val filesHere = (new java.io.File(".")).listFiles
-	for (file <- filesHere if file.getName.endsWith(".scala"))
-		println(file)
-	//还可以加入多个过滤条件，用;隔开
-	for (
-		file <- filesHere if file.isFile;
-		if file.getName.endsWith(".scala")
-	) println(file)
+  //在for循环结构中还可以加入if进行过滤操作
+  val filesHere = new java.io.File(".").listFiles
 
-	//多重循环的实现：
-	def fileLines(file: java.io.File) =
-		scala.io.Source.fromFile(file).getLines.toList
+  for (file <- filesHere if file.getName.endsWith(".scala"))
+    println(file)
+  //还可以加入多个过滤条件，用;隔开
+  for (file <- filesHere if file.isFile; if file.getName.endsWith(".scala")) println(file)
 
-	def grep(pattern: String) =
-		for (
-			file <- filesHere if file.getName.endsWith(".scala");
-			line <- fileLines(file) if line.trim.matches(pattern)
-		) println(file + ": " + line.trim)
+  //多重循环的实现：
+  def fileLines(file: java.io.File) = scala.io.Source.fromFile(file).getLines.toList
 
-	grep(".*gcd.*")
+  def grep(pattern: String) =
+    for (
+      file <- filesHere if file.getName.endsWith(".scala");
+      line <- fileLines(file) if line.trim.matches(pattern)
+    ) println(file + ": " + line.trim)
 
-	def scalaFiles =
-		for {
-			file <- filesHere
-			if file.getName.endsWith(".scala")
-		} yield file
+  grep(".*gcd.*")
+
+  def scalaFiles =
+    for {
+      file <- filesHere
+      if file.getName.endsWith(".scala")
+    } yield file
 }
--- src/main/scala/chapter1/control/MultFor.scala
+++ src/main/scala/chapter1/control/MultFor.scala
@@ -1,34 +1,34 @@
 package chapter1.control
 
+import java.io.File
+
 /**
   * Created by hadoop on 16-10-2.
   */
 object MultFor {
-	val filesHere = (new java.io.File("src/chapter1/control")).listFiles
+  val filesHere: Array[File] = new java.io.File("src/main/scala/chapter1/control").listFiles
 
-	def fileLines(file: java.io.File) =
-		scala.io.Source.fromFile(file).getLines.toList
+  def fileLines(file: java.io.File): List[String] =
+    scala.io.Source.fromFile(file).getLines.toList
 
-	//多重循环的实现：
-	def grep(pattern: String) =
-		for (
-			file <- filesHere if file.getName.endsWith(".scala");
-			line <- fileLines(file) if line.trim.matches(pattern)
-		)
-			println(file + ": " + line.trim)
+  //多重循环的实现：
+  def grep(pattern: String): Unit =
+    for (
+      file <- filesHere if file.getName.endsWith(".scala");
+      line <- fileLines(file) if line.trim.matches(pattern)
+    )
+      println(file + ": " + line.trim)
 
-	//等同于for嵌套
-	def grep2(pattern: String) =
-		for (
-			file <- filesHere if file.getName.endsWith(".scala")
-		) for (
-			line <- fileLines(file) if line.trim.matches(pattern)
-		) println(file + ": " + line.trim)
+  //等同于for嵌套
+  def grep2(pattern: String): Unit =
+    for (file <- filesHere if file.getName.endsWith(".scala"))
+      for (line <- fileLines(file) if line.trim.matches(pattern))
+        println(file + ": " + line.trim)
 
 
-	def main(args: Array[String]) {
-		grep(".*gcd.*")
-		println("----------------------------")
-		grep2(".*gcd.*")
-	}
+  def main(args: Array[String]) {
+    grep(".*gcd.*")
+    println("----------------------------")
+    grep2(".*gcd.*")
+  }
 }
